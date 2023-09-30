88c75d8c5b6a26bd9b25dcf4b82089fae7eca632
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-30 20:55:04 +0600

    try google auth

--- config/authGoogle.js
+++ config/authGoogle.js
@@ -7,7 +7,8 @@ const strategy = new GoogleStrategy(
     {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3001/auth/google/redirect",
+        callbackURL:
+            "https://bohubrihi-e-com-backend-app-2.onrender.com/auth/google/redirect",
     },
     async (accessToken, refreshToken, profile, cb) => {
         console.log("This is the user profile => ", profile);
@@ -50,13 +51,69 @@ const strategy = new GoogleStrategy(
         }
     }
 );
-
-// passport.serializeUser((user, done) => {
-//     done(null, user.id);
-// });
-
-// passport.deserializeUser((id, done) => {
-//     User.findById(id, (err, user) => done(err, user));
-// });
-
 passport.use(strategy);
+
+//! ---------------- Active ----------------
+
+// const passport = require("passport");
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const _ = require("lodash");
+// const { User } = require("../models/user");
+
+// const strategy = new GoogleStrategy(
+//     {
+//         clientID: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//         callbackURL: "http://localhost:3001/auth/google/redirect",
+//     },
+//     async (accessToken, refreshToken, profile, cb) => {
+//         console.log("This is the user profile => ", profile);
+
+//         let user = await User.findOne({
+//             googleId: profile.id,
+//             email: profile._json.email,
+//         });
+
+//         if (user) {
+//             const token = user.generateJWT();
+
+//             const response = {
+//                 user: _.pick(user, ["email", "_id"]),
+//                 token: token,
+//             };
+
+//             cb(null, response);
+
+//             console.log("User exists :", user);
+//         } else {
+//             user = new User({
+//                 googleId: profile.id,
+//                 email: profile._json.email,
+//                 name: profile.displayName,
+//             });
+
+//             await user.save();
+
+//             const token = user.generateJWT();
+
+//             const response = {
+//                 user: _.pick(user, ["email", "_id"]),
+//                 token: token,
+//             };
+
+//             cb(null, response);
+
+//             console.log("This is new user => ", user);
+//         }
+//     }
+// );
+
+// // passport.serializeUser((user, done) => {
+// //     done(null, user.id);
+// // });
+
+// // passport.deserializeUser((id, done) => {
+// //     User.findById(id, (err, user) => done(err, user));
+// // });
+
+// passport.use(strategy);
--- routers/socialAuthRouter.js
+++ routers/socialAuthRouter.js
@@ -24,14 +24,14 @@ router.get("/login/failed", (req, res) => {
     });
 });
 
-//* http://localhost:3001/auth/google
+//* https://bohubrihi-e-com-backend-app-2.onrender.com/auth/google
 router
     .route("/google")
     .get(passport.authenticate("google", { scope: ["profile", "email"] }));
 
-//* http://localhost:3001/auth/google/redirect
+//* https://bohubrihi-e-com-backend-app-2.onrender.com/auth/google/redirect
 
-const CLIENT_URL = "http://localhost:3000/login";
+const CLIENT_URL = "https://bohubrihi-e-com-backend-app-2.onrender.com/login";
 
 router.route("/google/redirect").get(
     passport.authenticate("google", {
@@ -48,9 +48,7 @@ router.route("/google/redirect").get(
 
 //todo ------------ FaceBook Authentication ---------------
 //* http://localhost:3001/auth/facebook
-router
-    .route("/facebook")
-    .get(passport.authenticate("facebook"));
+router.route("/facebook").get(passport.authenticate("facebook"));
 
 //* http://localhost:3001/auth/facebook/redirect
 router.route("/facebook/redirect").get(
@@ -66,3 +64,74 @@ router.route("/facebook/redirect").get(
 );
 
 module.exports = router;
+
+//! --------------------------- Active --------------------
+
+// const router = require("express").Router();
+// const passport = require("passport");
+// require("../config/authGoogle");
+// require("../config/authFaceBook");
+
+// //todo ------------ Google Authentication ---------------
+
+// // router.get("/login/success", (req, res) => {
+// //     console.log("This is successful login => ", req.user);
+// //     if (req.user) {
+// //         res.status(200).send({
+// //             success: true,
+// //             message: "successful",
+// //             user: req.user,
+// //             //   cookies: req.cookies
+// //         });
+// //     }
+// // });
+
+// router.get("/login/failed", (req, res) => {
+//     res.status(401).send({
+//         success: false,
+//         message: "failure",
+//     });
+// });
+
+// //* http://localhost:3001/auth/google
+// router
+//     .route("/google")
+//     .get(passport.authenticate("google", { scope: ["profile", "email"] }));
+
+// //* http://localhost:3001/auth/google/redirect
+
+// const CLIENT_URL = "http://localhost:3000/login";
+
+// router.route("/google/redirect").get(
+//     passport.authenticate("google", {
+//         session: false,
+//         failureRedirect: "/login/failed",
+//         // successRedirect: CLIENT_URL,
+//     }),
+//     (req, res) => {
+//         console.log("The user => ", req.user);
+//         const token = req.user.token;
+//         res.redirect(`${CLIENT_URL}?token=${token}`);
+//     }
+// );
+
+// //todo ------------ FaceBook Authentication ---------------
+// //* http://localhost:3001/auth/facebook
+// router
+//     .route("/facebook")
+//     .get(passport.authenticate("facebook"));
+
+// //* http://localhost:3001/auth/facebook/redirect
+// router.route("/facebook/redirect").get(
+//     passport.authenticate("facebook", {
+//         session: false,
+//         failureRedirect: "/login/failed",
+//     }),
+//     (req, res) => {
+//         console.log("The facebook user: ", req.user);
+//         const token = req.user.token;
+//         res.redirect(`${CLIENT_URL}?token=${token}`);
+//     }
+// );
+
+// module.exports = router;
