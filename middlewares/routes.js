1712d686927219411d5da5747997b91006c53194
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-09-30 21:45:46 +0600

    Back old V.

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -3,7 +3,7 @@ const categoryRouter = require("../routers/categoryRouter");
 const productRouter = require("../routers/productRouter");
 const cartRouter = require("../routers/cartRouter");
 const profileRouter = require("../routers/profileRouter");
-const paymentRouter = require("../routers/paymentRouter");
+// const paymentRouter = require("../routers/paymentRouter");
 //todo ==> modifications
 const commentRouter = require("../routers/commentRouter");
 const orderRouter = require("../routers/orderRouter");
@@ -16,7 +16,7 @@ module.exports = (app) => {
     app.use("/api/product", productRouter);
     app.use("/api/cart", cartRouter);
     app.use("/api/profile", profileRouter);
-    app.use("/api/payment", paymentRouter);
+    // app.use("/api/payment", paymentRouter);
     //todo ==> modifications
     app.use("/api/comment", commentRouter);
     app.use("/api/orders", orderRouter);
