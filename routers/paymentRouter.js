e736e6b2177fccf0cfe075b93114c2972b1a15b3
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-11-23 21:51:02 +0600

    payment system added

--- routers/paymentRouter.js
+++ routers/paymentRouter.js
@@ -1,13 +1,13 @@
-// const router = require("express").Router();
-// const {
-//     initPayment,
-//     ipn,
-//     paymentSuccess,
-// } = require("../controllers/paymentController");
-// const authorize = require("../middlewares/authorize");
+const router = require("express").Router();
+const {
+    initPayment,
+    ipn,
+    paymentSuccess,
+} = require("../controllers/paymentController");
+const authorize = require("../middlewares/authorize");
 
-// router.route("/").get(authorize, initPayment);
-// router.route("/ipn").post(ipn);
-// router.route("/success").post(paymentSuccess);
+router.route("/").get(authorize, initPayment);
+router.route("/ipn").post(ipn);
+router.route("/success").post(paymentSuccess);
 
-// module.exports = router;
+module.exports = router;
